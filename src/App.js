2f1a5e8c68677136e2102f673e99e407cff74a01
al_dente0 <al_dente0@whatever.com>
2022-02-13 10:16:29 +0300

    little changes

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import { NotFound } from './components/NotFound';
 function App() {
 	return (
 		<>
-			<Router>
+			<Router basename='/react-recipe'>
 				<Header />
 				<main className='container content'>
 					<Switch>
