6a59af78e350cc6522ff678e66e4452a17804751
al_dente0 <al_dente0@whatever.com>
2022-02-13 16:39:43 +0300

    add hamburger on mobile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,42 @@
-import {useState} from 'react';
 import {Link} from 'react-router-dom';
+import {useState} from 'react';
+
+/* import { Hamburger } from './Hamburger'; */
 
 
 export function Header() {
+    const [hamburgerActive, setHamburgerActive] = useState(false);
+
+    const handleClick = () => {
+        setHamburgerActive(!hamburgerActive);
+    }
+
 
     return (
         <header>
               <nav className="#66bb6a green lighten-1">
             <div className="nav-wrapper ">
                 <Link to='/' className="brand-logo left">React Recipe</Link>
-                    <ul id="nav-mobile" className="right">
-                        <li><Link to='/contacts'>Contacts</Link></li>
-                        <li><Link to="/about" >About</Link></li>
+                    <ul id="nav-mobile ul" className="right">
+                        <li className='dekstop'><Link to='/contacts'>Contacts</Link></li>
+                        <li className='dekstop'><Link to="/about" >About</Link></li>
+                        <li className="mobile" onClick={handleClick}>
+                            {
+                                !hamburgerActive ? <i className="material-icons">dehaze</i>
+                                : <i className="material-icons">close</i>
+                            }
+                        </li>
+                        
                     </ul>
+                    {
+                        hamburgerActive ? <div className="collection hamburger">
+                        <Link to="/contacts" className="collection-item" onClick={handleClick}>Contact</Link>
+                        <Link to="/about" className="collection-item" onClick={handleClick}>About</Link>
+                        </div> : null
+                    }
                 </div>
             </nav>
+            
         </header>
     )
 }
