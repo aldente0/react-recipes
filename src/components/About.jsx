91ce6032a4528b9da495832be7f97c8793d6ecc7
al_dente0 <al_dente0@whatever.com>
2022-02-14 09:50:27 +0300

    add links on another project

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,8 +4,13 @@ function About() {
             <h2>About project</h2>
             <p>React Recipe - my first SPA project and third project, that has made with react.<br />
             This is a recipe catalog.<br />
-            I'm Alexey Lozhkin - young beginner frontend delveloper.
-            repository on github: <a href="https://github.com/aldente0/react-recipes" target='_blank' rel="noreferrer">Repo</a>
+            I'm Alexey Lozhkin - young beginner frontend delveloper. <br />
+            Another projects on react: <br /><br />
+            Movie catalog: <a href="https://aldente0.github.io/react-movies/" target='_blank' rel="noreferrer">React Movie</a><br />
+            Github repository: <a href="https://github.com/aldente0/react-movies" target='_blank' rel="noreferrer">React Movie Github</a><br />
+            <br /><br />
+            Fortnite skin catalog: <a href="https://aldente0.github.io/react-shop/" target='_blank' rel="noreferrer">React Shop</a><br />
+            Github repository: <a href="https://github.com/aldente0/react-shop" target='_blank' rel="noreferrer">React Shop Github</a><br />
             </p>
         </>
         
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -14,26 +14,26 @@ export function Header() {
 
     return (
         <header>
-              <nav className="#66bb6a green lighten-1">
-            <div className="nav-wrapper ">
-                <Link to='/' className="brand-logo left">React Recipe</Link>
-                    <ul id="nav-mobile ul" className="right">
-                        <li className='dekstop'><Link to='/contacts'>Contacts</Link></li>
-                        <li className='dekstop'><Link to="/about" >About</Link></li>
-                        <li className="mobile" onClick={handleClick}>
-                            {
-                                !hamburgerActive ? <i className="material-icons">dehaze</i>
-                                : <i className="material-icons">close</i>
-                            }
-                        </li>
-                        
-                    </ul>
-                    {
-                        hamburgerActive ? <div className="collection hamburger">
-                        <Link to="/contacts" className="collection-item" onClick={handleClick}>Contact</Link>
-                        <Link to="/about" className="collection-item" onClick={handleClick}>About</Link>
-                        </div> : null
-                    }
+            <nav className="#66bb6a green lighten-1">
+                <div className="nav-wrapper ">
+                    <Link to='/' className="brand-logo left">React Recipe</Link>
+                        <ul id="nav-mobile ul" className="right">
+                            <li className='dekstop'><Link to='/contacts'>Contacts</Link></li>
+                            <li className='dekstop'><Link to="/about" >About</Link></li>
+                            <li className="mobile" onClick={handleClick}>
+                                {
+                                    !hamburgerActive ? <i className="material-icons">dehaze</i>
+                                    : <i className="material-icons">close</i>
+                                }
+                            </li>
+                            
+                        </ul>
+                        {
+                            hamburgerActive ? <div className="collection hamburger">
+                            <Link to="/contacts" className="collection-item" onClick={handleClick}>Contact</Link>
+                            <Link to="/about" className="collection-item" onClick={handleClick}>About</Link>
+                            </div> : null
+                        }
                 </div>
             </nav>
             
